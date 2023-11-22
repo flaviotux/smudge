dfccfe30a3af0713a2f93aad520e71f0d27f3f05
Flavio Ribeiro <flavio.ribeiro@luizalabs.com>
2023-11-22 15:00:49 -0300

    chore: add database methods to models

--- internal/model/todo.go
+++ internal/model/todo.go
@@ -1,8 +1,90 @@
 package model
 
+import (
+	"context"
+
+	"github.com/scylladb/gocqlx/v2"
+	"github.com/scylladb/gocqlx/v2/table"
+	"gitlab.luizalabs.com/luizalabs/smudge/db"
+)
+
 type Todo struct {
+	session *gocqlx.Session
+	table   table.Table
+
 	ID     string `json:"id" db:"id"`
 	Text   string `json:"text" db:"text"`
 	Done   bool   `json:"done" db:"done"`
 	UserID string `json:"userId" db:"user_id"`
+	User   *User
+}
+
+func NewTodoModel(session *gocqlx.Session) *Todo {
+	return &Todo{session: session, table: *db.TodoTable}
+}
+
+func (m *Todo) SetID(id string) *Todo {
+	m.ID = id
+	return m
+}
+
+func (m *Todo) SetText(text string) *Todo {
+	m.Text = text
+	return m
+}
+
+func (m *Todo) SetDone(done bool) *Todo {
+	m.Done = done
+	return m
+}
+
+func (m *Todo) SetUserID(userID string) *Todo {
+	m.UserID = userID
+	return m
+}
+
+func (m *Todo) SetUser(user *User) *Todo {
+	m.User = user
+	return m
+}
+
+func (m *Todo) GetQueryContext(ctx context.Context, columns ...string) (*Todo, error) {
+	uq := m.table.GetQueryContext(ctx, *m.session, columns...).BindStruct(m)
+	if err := uq.Get(&m); err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func (m *Todo) InsertQueryContext(ctx context.Context) (*Todo, error) {
+	q := m.table.InsertQueryContext(ctx, *m.session).BindStruct(m)
+	if err := q.ExecRelease(); err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func (m *Todo) SelectQueryContext(ctx context.Context, arg map[string]interface{}, columns ...string) ([]*Todo, error) {
+	var todo []*Todo
+
+	q := m.table.SelectQueryContext(ctx, *m.session).BindMap(arg)
+	if err := q.SelectRelease(&todo); err != nil {
+		return nil, err
+	}
+
+	return todo, nil
+}
+
+func (m *Todo) WithUserContext(ctx context.Context, columns ...string) (*Todo, error) {
+	u := NewUserModel(m.session).SetID(m.UserID)
+
+	if _, err := u.GetQueryContext(ctx, columns...); err != nil {
+		return nil, err
+	}
+
+	m.SetUser(u)
+
+	return m, nil
 }
--- internal/model/user.go
+++ internal/model/user.go
@@ -1,6 +1,60 @@
 package model
 
+import (
+	"context"
+
+	"github.com/scylladb/gocqlx/v2"
+	"github.com/scylladb/gocqlx/v2/table"
+	"gitlab.luizalabs.com/luizalabs/smudge/db"
+)
+
 type User struct {
+	session *gocqlx.Session
+	table   table.Table
+
 	ID   string `json:"id" db:"id"`
 	Name string `json:"name" db:"name"`
+}
+
+func NewUserModel(session *gocqlx.Session) *User {
+	return &User{session: session, table: *db.UserTable}
+}
+
+func (m *User) SetID(id string) *User {
+	m.ID = id
+	return m
+}
+
+func (m *User) SetName(name string) *User {
+	m.Name = name
+	return m
+}
+
+func (m *User) GetQueryContext(ctx context.Context, columns ...string) (*User, error) {
+	uq := m.table.GetQueryContext(ctx, *m.session, columns...).BindStruct(m)
+	if err := uq.Get(&m); err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func (m *User) InsertQueryContext(ctx context.Context) (*User, error) {
+	q := m.table.InsertQueryContext(ctx, *m.session).BindStruct(m)
+	if err := q.ExecRelease(); err != nil {
+		return nil, err
+	}
+
+	return m, nil
+}
+
+func (m *User) SelectQueryContext(ctx context.Context, arg map[string]interface{}, columns ...string) ([]*User, error) {
+	var user []*User
+
+	q := m.table.SelectQueryContext(ctx, *m.session, columns...).BindMap(arg)
+	if err := q.SelectRelease(&user); err != nil {
+		return nil, err
+	}
+
+	return user, nil
 }
